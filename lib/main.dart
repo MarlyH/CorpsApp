0636e73a74329d92d165e26d19bcee071f66a56d
MarlyH <161419386+MarlyH@users.noreply.github.com>
2025-05-25 19:09:38 +1200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'views/landing_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LandingView(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/views/landing_view.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class LandingView extends StatelessWidget {
+  const LandingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logo.png',
+                  height: 150,
+                ),
+                const SizedBox(height: 40),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // TODO: Navigate to login screen
+                    },
+                    child: const Text('Login'),
+                  ),
+                ),
+                const SizedBox(height: 12),
+                SizedBox(
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      // TODO: Navigate to register screen
+                    },
+                    child: const Text('Register'),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
+                    // TODO: Handle guest login
+                  },
+                  child: const Text(
+                    'Login as Guest',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

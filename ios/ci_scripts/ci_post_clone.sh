9e74ce55f7151f9bdee8f6cde132e9673fd0077f
Vivi9tt <tingtingyc99@gmail.com>
2025-12-27 18:22:37 +1300

    Update ci_post_clone.sh

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -3,13 +3,21 @@
 # Fail on error
 set -e
 
-# The path to your flutter installation is handled by Xcode Cloud if 
-# you've configured the environment, but it's safer to ensure 
-# dependencies are fetched.
-cd ../..
+# Xcode Cloud executes from the 'ci_scripts' directory.
+# Move to the root of the repository (usually 2 levels up from ci_scripts)
+cd "$(dirname "$0")/../.."
+
+# Install Flutter via homebrew if it's not detected (Xcode Cloud has Homebrew)
+if ! command -v flutter &> /dev/null
+then
+    echo "Flutter not found, installing via Homebrew..."
+    brew install --cask flutter
+fi
+
+# Precache and get packages
 flutter precache --ios
 flutter pub get
 
-# Navigate to the iOS folder to install pods
+# Navigate to the iOS folder and install pods
 cd ios
 pod install
